71e1b531962fe147d46d9277473dd47d8b9e21f6
marwawahid-cyber <marwawahid99@gmail.com>
2026-02-27 09:30:57 +0500

    Added Day 1 introduction to bash script

---
+++ intro-to-bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Day 1 - Introduction to Bash
+
+echo "My current shell is:"
+echo $SHELL
+
+echo "Displaying environment variables:"
+env
+
+echo "Location of sh shell:"
+which sh
+
+echo "This is my first Bash practice script"
